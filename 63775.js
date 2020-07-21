8fd6b578fa4fa6713ac5efbdd5c690507828f79a
prodesain <58811834+prodesain@users.noreply.github.com>
2020-07-22 03:00:22 +0700

    Add files via upload

---
+++ 63775.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://bit.ly/3eMpuLO', '_blank');
+	});
+</script>
